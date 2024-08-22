a6d4fb39ac3e311e640e11165f25b7de235fb7f7
felipe <neivajansen.felipe@gmail.com>
2024-08-22 09:46:13 -0300

    https://neetcode.io/problems/duplicate-integer

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,25 @@
-
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 // https://neetcode.io/problems/duplicate-integer
 public class Main {
     public static void main(String[] args) {
-// Define um array de inteiros contendo os números {1, 2, 3, 3}.
-        int[] nums= {1,2,3,3};
+        // Define um array de inteiros contendo os números {1, 2, 3, 3}.
+        int[] nums = {1, 2, 3, 3};
 
-// Cria um HashMap para armazenar cada número do array como chave e sua contagem como valor.
+        // Cria um HashMap para armazenar cada número do array como chave e sua contagem como valor.
         HashMap<Integer, Integer> map = new HashMap<>();
 
-// Itera sobre cada número do array 'nums'.
-        for (int numero: nums) {
+        // Itera sobre cada número do array 'nums'.
+        for (int numero : nums) {
             // Para cada número, verifica se já existe no HashMap.
             // Se existir, incrementa o valor associado à chave (contagem do número).
             // Se não existir, adiciona o número como chave com valor inicial 1.
             map.put(numero, map.getOrDefault(numero, 0) + 1);
         }
 
-// Usa a API de Streams para buscar a primeira entrada no HashMap
-// onde o valor (contagem) é maior que 1, ou seja, encontra o primeiro número duplicado.
-        System.out.println(map.entrySet()
-                .stream()
-                .anyMatch(entry -> entry.getValue() > 1));
+        // Usa a API de Streams para buscar a primeira entrada no HashMap
+        // onde o valor (contagem) é maior que 1, ou seja, encontra o primeiro número duplicado.
+        System.out.println(map.entrySet().stream().anyMatch(entry -> entry.getValue() > 1));
 
     }
 }
