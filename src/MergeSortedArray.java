263ed8b2f668a4426f39f6eba9ac3d47f285dfa4
felipe <neivajansen.felipe@gmail.com>
2024-08-28 00:18:46 -0300

    https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150

---
+++ src/MergeSortedArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+// https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+
+        for (int i = m, j = 0; i < m + n; i++, j++) {
+            nums1[i] = nums2[j];
+        }
+        Arrays.sort(nums1);
+    }
+}
